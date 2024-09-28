7baa660c1f844435bbad5cae23aa2e5b2524d078
Martin Fernandez <martinnfernandezz.20@gmail.com>
2024-09-28 16:13:05 -0300

    Agrego nuevos colores img, gif a inicio

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'; // Aquí puedes agregar estilos propios
 import NavBar from "./components/NavBar.jsx";
 import ItemListContainer from './components/ItemListContainer';
+import HeroSection from './components/HeroSection.jsx';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer  greeting="¡Bienvenidos a nuestro E-commerce!" /> 
+      <ItemListContainer  greeting="¡Bienvenidos a Vapeo 3.5!" /> 
+      <HeroSection></HeroSection>
     </div>
   );
 }
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+      <div className="hero-section">
+        <div className="hero-content">
+          <h2>Descubre un mundo de sabores y alternativas</h2>
+          <a href="/productos" className="explore-button">Explorar</a>
+        </div>
+      </div>
+    );
+  };
+
+export default HeroSection
